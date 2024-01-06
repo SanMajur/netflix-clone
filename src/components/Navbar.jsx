086ed84b60c9a1dfe566f82e734d846879c1e3e0
santino majur <demalong22@gmail.com>
2024-01-06 17:25:02 +0200

    authentication with firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
-  const {user, logOut} = UserAuth()
-  console.log(user)
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  //console.log(user.email)
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
       <Link to="/">
@@ -12,16 +21,30 @@ const Navbar = () => {
           Netflix
         </h1>
       </Link>
-      <div className="">
-        <Link to="/login">
-          <button className="text-white pr-4 capitalize">sign in</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer capitalize">
-            sign up
+      {user?.email ? (
+        <div className="">
+          <Link to="/account">
+            <button className="text-white pr-4 capitalize">Account</button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer capitalize text-white"
+          >
+            Logout
           </button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div className="">
+          <Link to="/login">
+            <button className="text-white pr-4 capitalize">sign in</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer capitalize text-white">
+              sign up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
+    const { user, logIn } = UserAuth();
+    const navigate = useNavigate()
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError('')
+      try {
+        await logIn(email, password);
+        navigate('/')
+      } catch (error) {
+        console.log(error);
+        setError(error.message)
+      }
+    };
   return (
     <div className='w-full h-screen'>
     <img className='hidden sm:block absolute w-full h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/0f291ec0-5299-466c-9695-4fe6de893979/SS-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="" />
@@ -10,9 +28,10 @@ const Login = () => {
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
                 <h1 className="text-3xl font-bold">Sign In</h1>
-                <form className='flex w-full flex-col py-4'>
-                    <input className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' autoComplete='email'/>
-                    <input className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
+                {error ? <p className='p-3 bg-red-400 my-2 rounded'>{error}</p> : null}
+                <form onSubmit={handleSubmit} className='flex w-full flex-col py-4'>
+                    <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' autoComplete='email'/>
+                    <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' autoComplete='current-password'/>
                     <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
                     <div className="flex items-center justify-between text-sm text-gray-600">
                         <p className='mr-2'><input type="checkbox" />Remember me</p>
