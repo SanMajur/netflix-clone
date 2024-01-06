3797ee4ff97672ff63d5d6af5885526581268cde
santino majur <demalong22@gmail.com>
2024-01-06 12:20:45 +0200

    signup component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+  console.log(user)
   return (
-    <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
-        <div className="">
-            <button className="text-white pr-4 capitalize">sign in</button>
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer capitalize">sign up</button>
-        </div>
+    <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          Netflix
+        </h1>
+      </Link>
+      <div className="">
+        <Link to="/login">
+          <button className="text-white pr-4 capitalize">sign in</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer capitalize">
+            sign up
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
